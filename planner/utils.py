28d88a963de1818a65a53bcef40b24704789675a
williamhuang08 <williamhuang1108@gmail.com>
2026-02-21 15:00:32 -0500

    Removed dataset functions from utils

--- planner/utils.py
+++ planner/utils.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import torch
 import imageio
 import numpy as np
-from torch.utils.data import DataLoader, Dataset
 import random
 
 
@@ -42,61 +41,3 @@ def make_episode_splits(minari_dataset, train=0.8, val=0.1, test=0.1, seed=0):
     test_ids = idxs[n_train+n_val:]
     return train_ids, val_ids, test_ids
 
-class SubtrajDataset(Dataset):
-    """
-    Loops over minari_dataset.iterate_episodes(), but keeps only episodes whose index is in episode_ids
-    """
-    def __init__(self, minari_dataset, T, episode_ids, stride=3):
-        self.T = T
-        self.items = [] 
-        self.removed_items = [] 
-
-        # Iterate all episodes but only process those whose global index is in episode_ids
-        for ep_idx, ep in enumerate(minari_dataset.iterate_episodes()):
-            if ep_idx not in set(episode_ids):
-                continue
-            obs = ep.observations["observation"]          
-            ach = ep.observations["achieved_goal"]        
-            act = ep.actions                               
-            Ltot = len(obs)
-            if Ltot < T + 1:
-                continue
-
-            state_ext = np.concatenate([obs, ach], axis=-1).astype(np.float32)
-            for t in range(0, Ltot - T, stride):
-                state_seq = state_ext[t:t+T]         
-                s0 = state_seq[0]             
-                action_seq = act[t:t+T].astype(np.float32)  
-                sT = state_ext[t+T]           
-                if np.linalg.norm(s0[-2:] - sT[-2:]) > 0.4:
-                    self.items.append((s0, state_seq, action_seq, sT))
-                else:
-                    self.removed_items.append((s0, state_seq, action_seq, sT))
-
-    def __len__(self): 
-        return len(self.items)
-
-    def __getitem__(self, i):
-        """standardize s0, state_sequence, and sT by (x - mean) / std"""
-        
-        s0, S, A, sT = self.items[i]
-        if hasattr(self, "stats") and self.stats is not None:
-            S_mean, S_std = self.stats
-            S  = (S  - S_mean) / S_std
-            s0 = (s0 - S_mean) / S_std
-            sT = (sT - S_mean) / S_std
-            A  = A
-        return {
-            "s0": torch.as_tensor(s0, dtype=torch.float32),
-            "state_sequence": torch.as_tensor(S, dtype=torch.float32),
-            "action_sequence": torch.as_tensor(A, dtype=torch.float32),
-            "sT": torch.as_tensor(sT, dtype=torch.float32),
-        }
-
-def collate(batch):
-    return {
-        "s0": torch.stack([b["s0"] for b in batch], 0),
-        "state_sequence": torch.stack([b["state_sequence"] for b in batch], 0),
-        "action_sequence": torch.stack([b["action_sequence"] for b in batch], 0),
-        "sT": torch.stack([b["sT"] for b in batch], 0),
-    }
