671d9af33b7d36916ea03c06f8d790274a4da6c5
williamhuang08 <williamhuang1108@gmail.com>
2026-03-27 20:34:20 -0400

    added obs decoder

--- model/online/models/__init__.py
+++ model/online/models/__init__.py
@@ -1,4 +1,9 @@
-from .rssm import AbstractRSSM
+from .segment_latent_model import (
+    StartStateEncoder,
+    StatePosteriorTransformer,
+    SegmentDynamics,
+)
+from .observation_decoder import SegmentObservationDecoder
 from .skill_encoder import TransformerSkillEncoder
 from .reward_model import RewardModel
 from .skill_prior import AbstractSkillPrior
